b0746fef0195e128fcda0fe297c5ef8299c12bc1
Anna Luisa Patiño West <aisapatino@gmail.com>
2015-01-30 14:28:23 -0800

    comments

--- check_coverage.sh
+++ check_coverage.sh
@@ -1,2 +1,9 @@
+#!/usr/bin/env bash
+
+# uses coverage package - https://pypi.python.org/pypi/coverage
+
+# run all tests with coverage wrapper
 coverage run --source='sjfnw' --omit='sjfnw/wsgi.py,sjfnw/mail.py,*/tests.py,*__init__.py,*commands/*' manage.py test grants fund
+
+# output html results in /coverage
 coverage html -d ~/Projects/coverage
