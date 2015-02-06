961535515b3852fab41983364398619c0a363447
aisa <aisapatino@gmail.com>
2015-02-05 21:36:17 -0800

    get unicode reps of models working more smoothly

--- sjfnw/fund/tests/test_models.py
+++ sjfnw/fund/tests/test_models.py
@@ -1,3 +1,5 @@
+# coding: utf8
+
 from django.test import TestCase
 from django.utils import timezone
 
@@ -6,15 +8,15 @@ from sjfnw.fund.models import GivingProject, Member, Membership
 class ModelUnicodes(TestCase):
 
   def test_unicode(self):
-    project = GivingProject(title='A Fake Giving Project',
-        fundraising_training=timezone.now(),
-        fundraising_deadline=timezone.now()
-      )
+    """ Verify that unicode methods work for Member, GP and Membership """
+    project = GivingProject(title=u'Â Fake Giving Project',
+                            fundraising_training=timezone.now(),
+                            fundraising_deadline=timezone.now())
     project.save()
-    member = Member(first_name='Al', last_name='Fisher')
+    member = Member(first_name='Al', last_name=u'Fiüsher')
     member.save()
     membership = Membership(giving_project=project, member=member)
-    self.assertEqual(u'Al Fisher', unicode(member))
-    self.assertEqual(u'A Fake Giving Project %d' % timezone.now().year, unicode(project))
-    self.assertEqual(u'Al Fisher, A Fake Giving Project %d' % timezone.now().year, unicode(membership))
+    self.assertEqual(u'Al Fiüsher', unicode(member))
+    self.assertEqual(u'Â Fake Giving Project %d' % timezone.now().year, unicode(project))
+    self.assertEqual(u'Al Fiüsher, Â Fake Giving Project %d' % timezone.now().year, unicode(membership))
 
