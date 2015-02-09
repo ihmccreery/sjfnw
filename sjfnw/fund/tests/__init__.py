ec298572662041e390e8e050bdff492242568f6b
aisa <aisapatino@gmail.com>
2015-02-08 17:17:52 -0800

    tests for new utils

--- sjfnw/fund/tests/__init__.py
+++ sjfnw/fund/tests/__init__.py
@@ -2,6 +2,7 @@
 from test_copy_contacts import *
 from test_grants import *
 from test_models import *
+from test_model_utils import *
 from test_personal import *
 from test_project_page import *
 from test_register import *
