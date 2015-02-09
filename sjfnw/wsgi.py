e117063e6565d0f6d891abeea757252220a5b33c
aisa <aisapatino@gmail.com>
2015-02-09 00:17:03 -0800

    consolidate log formatting/config

--- sjfnw/wsgi.py
+++ sjfnw/wsgi.py
@@ -1,25 +1,15 @@
-from django.core.wsgi import get_wsgi_application
-from django.core.signals import got_request_exception
-import logging
 import os, sys
 
-# set formatting for logging
-datefmt = '%Y-%m-%d %H:%M:%S'
-if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
-  fr = logging.Formatter(fmt='[%(filename)s:%(lineno)d %(funcName)s]: %(message)s', datefmt=datefmt)
-else:
-  fr = logging.Formatter(fmt='%(levelname)-8s %(asctime)s %(filename)s:%(lineno)d %(funcName)s]: %(message)s', datefmt=datefmt)
-handlers = logging.getLogger().handlers
-if handlers:
-  handlers[0].setFormatter(fr)
+from django.core.wsgi import get_wsgi_application
+from django.core.signals import got_request_exception
+
+from sjfnw.log import configure_logging, log_exception
 
 # path & env vars
 sys.path.append(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sjfnw.settings")
 
-# log errors
-def log_exception(*args, **kwds):
-  logging.exception('Exception in request:')
+configure_logging()
 got_request_exception.connect(log_exception)
 
 # define wsgi app
