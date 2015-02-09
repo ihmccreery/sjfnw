a650c7f72de75264e739eef2de960e7023532a5b
aisa <aisapatino@gmail.com>
2015-02-09 00:32:12 -0800

    fix log

--- sjfnw/log.py
+++ sjfnw/log.py
@@ -1,10 +1,5 @@
 import logging, os, sys
 
-RED = '\033[00;31m'
-YELLOW = '\033[00;33m'
-RESET '\033[00m'
-BOLD= '\033[1m'
-
 def configure_logging():
   datefmt = '%Y-%m-%d %H:%M:%S'
   if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
@@ -19,7 +14,6 @@ def configure_logging():
   else:
     sys.stdout.write('basicconfig')
     logging.basicConfig(format=log_format, datefmt=datefmt)
-
 
 # log errors
 def log_exception(*args, **kwds):
