c02be5edd06e6f8c55f078e8a805a129399d6576
aisa <aisapatino@gmail.com>
2015-02-08 17:17:02 -0800

    splitting views into functions
    modelutils separate from utils to avoid circular imports
    i #702

--- sjfnw/fund/utils.py
+++ sjfnw/fund/utils.py
@@ -1,8 +1,5 @@
-﻿from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
-from django.http import HttpResponse
 from django.template.loader import render_to_string
-from django.utils import timezone
 from django.utils.html import strip_tags
 
 from sjfnw import constants
