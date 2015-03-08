8043d523bf6741c4729d0468310fb6f51f3c8824
aisa <aisapatino@gmail.com>
2015-03-07 17:12:00 -0800

    clearer css class names

--- sjfnw/fund/forms.py
+++ sjfnw/fund/forms.py
@@ -64,7 +64,7 @@ class MassDonorPre(forms.Form):
 class MassDonor(MassDonorPre):
   amount = IntegerCommaField(label='*Estimated donation ($)',
                              min_value=0,
-                             widget=forms.TextInput(attrs={'class':'tq'}))
+                             widget=forms.TextInput(attrs={'class':'width-75'}))
   likelihood = forms.IntegerField(label='*Estimated likelihood (%)',
                                   min_value=0, max_value=100,
                                   widget=forms.TextInput(attrs={'class':'half'}))
@@ -75,7 +75,7 @@ class DonorEstimates(forms.Form):
                                  widget=forms.HiddenInput())
   amount = IntegerCommaField(label='*Estimated donation ($)',
                              min_value=0,
-                             widget=forms.TextInput(attrs={'class':'tq'}))
+                             widget=forms.TextInput(attrs={'class':'width-75'}))
   likelihood = forms.IntegerField(label='*Estimated likelihood (%)',
                                   min_value=0, max_value=100,
                                   widget=forms.TextInput(attrs={'class':'half'}))
