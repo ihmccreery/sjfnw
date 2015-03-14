d8b803c4e37774879f2cc66a1f2448525c49fdd4
aisa <aisapatino@gmail.com>
2015-03-13 17:36:39 -0700

    cleanup, docstrings

--- sjfnw/forms.py
+++ sjfnw/forms.py
@@ -1,9 +1,14 @@
+import re
+
 from django import forms
 from django.core import validators
 from django.core.exceptions import ValidationError
-import re
+
 
 class PhoneNumberField(forms.Field):
+  """ Accepts 10-digit phone numbers regardless of input format
+      Standardizes to xxx-xxx-xxxx format """
+
   default_error_messages = {
     'invalid': u'Enter a 10-digit phone number.',
   }
@@ -11,30 +16,36 @@ class PhoneNumberField(forms.Field):
   def __init__(self, *args, **kwargs):
     super(PhoneNumberField, self).__init__(*args, **kwargs)
 
+  def clean(self, value):
+    """ Strip extraneous characters before proceeding with validation """
+
+    if isinstance(value, (str, unicode)):
+      value = re.sub(ur'[()\-\s\u2010]', '', value) # u2010 is hyphen
+      if value and len(value) != 10:
+        raise ValidationError(self.error_messages['invalid'])
+
+    return super(PhoneNumberField, self).clean(value)
+
   def to_python(self, value):
-    """
-    Validates that int() can be called on the input.
-    Returns '' for empty values.
-    """
+    """ Validate that input can be converted to integer
+        Return phone number as a formatted string, or '' if empty """
+
     value = super(PhoneNumberField, self).to_python(value)
     if value in validators.EMPTY_VALUES:
       return ''
-    try:
+
+    try: #TODO this probably belongs elsewhere since we're not using the int value
       int(str(value))
     except (ValueError, TypeError):
       raise ValidationError(self.error_messages['invalid'])
+
     return value[:3] + u'-' + value[3:6] + u'-' + value[6:]
 
-  def clean(self, value):
-    if isinstance(value, (str, unicode)):
-      value = re.sub(ur'[()\-\s\u2010]', '', value) #u2010 = hyphen
-      if value and len(value) != 10:
-        raise ValidationError(self.error_messages['invalid'])
-    return super(PhoneNumberField, self).clean(value)
 
 class IntegerCommaField(forms.Field):
-  """ Allows commas separating thousands
-  (Mostly copied from IntegerField) """
+  """ Accepts numbers formatted with commas. Stores them as integers.
+
+     (Mostly copied from IntegerField, with some modifications) """
 
   default_error_messages = {
     'invalid': u'Enter a whole number. (Format: 11009 or 11,009)',
@@ -51,31 +62,27 @@ class IntegerCommaField(forms.Field):
     if min_value is not None:
       self.validators.append(validators.MinValueValidator(min_value))
 
+  def clean(self, value):
+    """ Remove commas and cents before proceeding with validation """
+
+    if isinstance(value, (str, unicode)):
+      value = value.replace(',', '')
+      amount = re.match(r'(\d+)(\.\d{2})?$', value)
+      if amount:
+        value = amount.group(1)
+
+    return super(IntegerCommaField, self).clean(value)
+
   def to_python(self, value):
-    """
-    Validates that int() can be called on the input. Returns the result
-    of int(). Returns None for empty values.
-    """
+    """ Coerce to integer or return None for empty values """
+
     value = super(IntegerCommaField, self).to_python(value)
     if value in validators.EMPTY_VALUES:
       return None
+
     try:
       value = int(str(value))
     except (ValueError, TypeError):
       raise ValidationError(self.error_messages['invalid'])
+
     return value
-
-  def clean(self, value):
-    """ Process the value prior to to_python
-      Removes commas and cents """
-    if isinstance(value, (str, unicode)):
-      value = value.replace(",", "")
-      amount = re.match(r'(\d+)(\.\d{2})?$', value)
-      if amount:
-        value = amount.group(1)
-    return super(IntegerCommaField, self).clean(value)
-
-class GaeLogsForm(forms.Form):
-  start = forms.SplitDateTimeField()
-  end = forms.SplitDateTimeField()
-  version_ids = forms.MultipleChoiceField(choices=[('1', '1'), ('devel', 'devel')])
