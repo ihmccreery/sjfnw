0ec7796a2860b648472416b89f86d455bd86d181
tarhata <tarhata.joy@gmail.com>
2015-03-15 12:19:00 -0700

    added match fields to Donor model

--- sjfnw/fund/models.py
+++ sjfnw/fund/models.py
@@ -288,11 +288,14 @@ class Donor(models.Model):
   received_afternext = models.PositiveIntegerField(default=0,
       verbose_name='Received - year after next')
   gift_notified = models.BooleanField(default=False)
+  match_expected = models.PositiveIntegerField(blank=True, default=0)
+  match_company = models.TextField(blank=True)
+  match_received = models.PositiveIntegerField(blank=True)
 
   # contact info only required if promise is entered
   phone = models.CharField(max_length=15, blank=True)
   email = models.EmailField(max_length=100, blank=True)
-  notes = models.TextField(blank=True, )
+  notes = models.CharField(max_length=100, blank=True)
 
   class Meta:
     ordering = ['firstname', 'lastname']
