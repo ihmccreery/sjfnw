174061dbcfec7e7b6d332a1fe32ecf8fb95ae28a
aisa <aisapatino@gmail.com>
2015-05-24 10:37:20 -0700

    minor code formatting

--- sjfnw/grants/tests/test_givingprojectgrants.py
+++ sjfnw/grants/tests/test_givingprojectgrants.py
@@ -1,5 +1,4 @@
-from datetime import timedelta, date
-import json
+from datetime import timedelta
 import logging
 
 from django.utils import timezone
@@ -10,15 +9,12 @@ from sjfnw.grants.tests.base import BaseGrantTestCase
 logger = logging.getLogger('sjfnw')
 
 class NewGivingProjectGrant(BaseGrantTestCase):
-  """ Test giving project grant methods:
-      yearend due, total_amount, and grant_length
-  """
+  """ Test GivingProjectGrant model methods """
 
   def setUp(self):
     super(NewGivingProjectGrant, self).setUp()
 
   def test_minimum_grant_information(self):
-
     award = models.GivingProjectGrant(projectapp_id=1, amount=5000)
     award.save()
 
@@ -34,41 +30,30 @@ class NewGivingProjectGrant(BaseGrantTestCase):
     self.assertEqual(award.agreement_returned, None)
     self.assertEqual(award.approved, None)
 
-
   def test_single_year_grant(self):
-    """ Verify year end due date, grant length, and grant amounts for a single year grant """
-
-    today = timezone.now().date()
     award = models.GivingProjectGrant(projectapp_id=1, amount=5000,
-                                      agreement_mailed=today)
-
+                                      agreement_mailed=timezone.now().date())
     award.save()
 
     self.assertEqual(award.total_amount(), award.amount)
     self.assertEqual(award.yearend_due(), award.agreement_mailed.replace(
-                     year=award.agreement_mailed.year + 1))
+                       year=award.agreement_mailed.year + 1))
     self.assertEqual(award.grant_length(), 1)
     self.assertEqual(award.second_check_mailed, None)
 
   def test_two_year_grant(self):
-    """ Verify year end due dates, grant length, and grant amounts for a two year grant """
-
     today = timezone.now().date()
-    award = models.GivingProjectGrant(projectapp_id=1, amount=5000, second_amount=5000,
-                                     agreement_mailed=today - timedelta(days=366))
+    award = models.GivingProjectGrant(projectapp_id=1, amount=5000,
+      second_amount=5000, agreement_mailed=today - timedelta(days=366))
     award.save()
 
     self.assertEqual(award.total_amount(), award.amount + award.second_amount)
     self.assertEqual(award.grant_length(), 2)
 
-    first_yearend = award.agreement_mailed.replace(year=award.agreement_mailed.year + 1)
-    self.assertEqual(award.yearend_due(), first_yearend)
+    first_yer_due = award.agreement_mailed.replace(year=award.agreement_mailed.year + 1)
+    self.assertEqual(award.yearend_due(), first_yer_due)
 
-    award.second_check_mailed=today
+    award.second_check_mailed = today
     award.save()
-    second_yearend = award.agreement_mailed.replace(year=award.agreement_mailed.year + 2)
-    self.assertEqual(award.yearend_due(), second_yearend)
-
-
-
-
+    second_yer_due = award.agreement_mailed.replace(year=award.agreement_mailed.year + 2)
+    self.assertEqual(award.yearend_due(), second_yer_due)
