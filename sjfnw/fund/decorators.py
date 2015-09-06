3cb37ec09d9dfae0bbb3a5e4dacaea1c79a450a6
Anna Luisa Patiño West <aisapatino@gmail.com>
2015-09-05 17:44:10 -0700

    cleanup membership middleware code

--- sjfnw/fund/decorators.py
+++ sjfnw/fund/decorators.py
@@ -1,29 +1,41 @@
-﻿from functools import wraps
+from functools import wraps
+import logging
+
 from django.shortcuts import redirect
 from django.utils.decorators import available_attrs
-import logging
+
+from sjfnw import constants as c
+
 logger = logging.getLogger('sjfnw')
 
-""" request.membership_status from middleware
-  3 = current membership is approved
-  2 = no approved memberships
-  1 = no memberships
-  0 = no member object
-"""
-
 def approved_membership(function=None):
+  """ Allows access to view only if request.membership_status is c.APPROVED
+
+    If not allowing access to view, redirects to appropriate settings/notice page
+
+    Should be used with @login_required
+    Relies on data set by MembershipMiddleware
+
+    Returns decorator for wrapping the view
+  """
+
   def decorator(view_func):
+
     @wraps(view_func, assigned=available_attrs(view_func))
     def _wrapped_view(request, *args, **kwargs):
-      if request.membership_status == 3: #success, just do the view func
+
+      if request.membership_status == c.APPROVED:
         return view_func(request, *args, **kwargs)
-      elif request.membership_status == 2: #membership(s) not approved
+
+      elif request.membership_status == c.NO_APPROVED:
         logger.info('Membership(s) not approved, redirecting to pending')
         return redirect('/fund/pending')
-      elif request.membership_status == 1: #no memberships
+
+      elif request.membership_status == c.NO_MEMBERSHIP:
         logger.info('No memberships, redirecting to projects page')
         return redirect('/fund/projects')
-      else: #no member object
+
+      else:
         return redirect('/fund/not-member')
 
     return _wrapped_view
