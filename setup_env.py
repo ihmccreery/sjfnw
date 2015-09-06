6d95ae0d51b52cd0e730652a30de5d04128a6e90
Anna Luisa Patiño West <aisapatino@gmail.com>
2015-09-05 17:00:23 -0700

    fix libs path

--- setup_env.py
+++ setup_env.py
@@ -5,7 +5,7 @@ setup_complete = False
 if not setup_complete:
   # path - add project root and libs directory
   sys.path.append(os.path.dirname(__file__))
-  sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'libs'))
+  sys.path.append(os.path.join(os.path.dirname(__file__), 'libs'))
 
   # env
   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sjfnw.settings')
