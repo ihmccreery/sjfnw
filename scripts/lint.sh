13cbfae96e27e1d4301da42f3136f7b1a17a21ca
Anna Luisa Patiño West <aisapatino@gmail.com>
2015-09-07 11:16:55 -0800

    Deployed dfc13b3 with MkDocs version: 0.14.0

--- scripts/lint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# run python linter (configured by .pylintrc)
-pylint --rcfile=.pylintrc --load-plugins=pylint_django sjfnw
-
-# run javascript linter (configured by .eslintrc)
-eslint sjfnw/static/js/*.js
