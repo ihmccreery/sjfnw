10b06cb12aebc21cfa44344d5459510e5d2d3c7e
Anna Luisa Patiño West <aisapatino@gmail.com>
2015-09-10 15:38:16 -0700

    remove unnecessary import

--- sjfnw/utils.py
+++ sjfnw/utils.py
@@ -1,6 +1,3 @@
-import logging
-logger = logging.getLogger('sjfnw')
-
 def create_link(url, text, new_tab=False):
   new_tab = ' target="_blank"' if new_tab else ''
   return '<a href="{}"{}>{}</a>'.format(url, new_tab, text)
