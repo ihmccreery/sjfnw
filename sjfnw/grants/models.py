e36dd662435508a2e99684f639726f769bfda482
Anna Luisa Patiño West <aisapatino@gmail.com>
2016-01-05 19:15:08 -0800

    add two year fields to grant cycle
    boolean for is two-year, textfield for extra question

--- sjfnw/grants/models.py
+++ sjfnw/grants/models.py
@@ -139,6 +139,16 @@ class GrantCycle(models.Model):
   private = models.BooleanField(default=False, verbose_name=
       'Private (will not be displayed to orgs, but can be accessed by '
        'anyone who has the direct link)')
+  two_year_grants = models.BooleanField(default=False,
+      verbose_name='Cycles associated with two-year grants have an extra '
+      'question in their application.')
+  two_year_question = models.TextField(blank=True, default=
+      'This grant will provide funding for two years. While we know it can be '
+      'difficult to predict your work beyond a year, please give us an idea of '
+      'what the second year might look like.<ol><li>What overall goals and '
+      'strategies do you forecast in the second year?</li><li>How will the '
+      'second year of this grant build on your work in the first year?</li></ol>',
+      verbose_name='This question is only shown if "Two year grants" is checked')
 
   class Meta:
     ordering = ['-close', 'title']
