12c7a9f400e1b62d485d88bba655fe17a5b12fdf
Anna Luisa Patiño West <aisapatino@gmail.com>
2016-06-19 10:11:08 -0700

    use app-specific base page for 404/500

--- sjfnw/views.py
+++ sjfnw/views.py
@@ -6,30 +6,37 @@ from django.shortcuts import render
 logger = logging.getLogger('sjfnw')
 
 def _get_context(path):
-  if path.find('/fund') == 0:
+
+  if path.startswith('/fund'):
+    base_template = 'fund/_base.html'
     title_addition = ' - Project Central'
     contact_url = '/fund/support'
-  elif path.find('/org') == 0 or path.find('/apply') == 0:
+
+  elif path.startswith('/apply') or path.startswith('/org'):
+    base_template = 'grants/base.html'
     title_addition = ' - Social Justice Fund Grants'
     contact_url = '/org/support'
+
   else:
+    base_template = 'base.html'
     title_addition = ' - Social Justice Fund Apps'
     contact_url = ''
 
   return {
+    'base_template': base_template,
     'title_addition': title_addition,
     'contact_url': contact_url
   }
 
 def page_not_found(request):
-  """ Modified version of default handler. Renders app-specific template. """
+  """ Custom 404 handler. Renders template with app-specific context. """
   page_context = _get_context(request.path)
   return http.HttpResponseNotFound(render(request, '404.html', page_context))
 
 def server_error(request):
-  """ Modified version of default handler. Renders app-specific template. """
+  """ Custom 500 handler. Renders template with app-specific context. """
   page_context = _get_context(request.path)
-  return http.HttpResponseNotFound(render(request, '500.html', page_context))
+  return http.HttpResponseServerError(render(request, '500.html', page_context))
 
 def log_javascript(request):
   """ Receives javascript errors and logs them """
