0870bc465f3101d61f2589fcd29b4c416d3dd696
Anna Luisa Patiño West <aisapatino@gmail.com>
2016-08-04 10:43:04 -0700

    update word count validator to ignore punctuation
    ignore punctuation-only words like hyphens, bullets, etc.

--- sjfnw/grants/tests/test_validators.py
+++ sjfnw/grants/tests/test_validators.py
@@ -1,7 +1,44 @@
+# encoding: utf-8
+
+import re
+
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from sjfnw.grants.models import WordLimitValidator
+
+def simple_clean(val):
+  """ Clean method used as of 9d8b1d9 (tag 2016-08-03)
+      Included here to ensure that count from new method is always lte old
+  """
+  return len(re.findall(r'[^ \n\r]+', val))
+
+# "simple" indicates whether the simple_clean method should give an accurate word count
+EXAMPLES = [
+  {
+    'text': ('Please fill in this timeline to describe your activities over the'
+      ' next five quarters. This will not exactly match up with the time period'
+      ' funded by this grant.\nWe are asking for this information to give us an idea'
+      ' of what-your work looks like: what you are doing and how those activities'
+      ' intersect and build on each other and move you towards your goals. Because'
+      ' our grants are usually general operating funds, we want to get a sense of'
+      ' what your organizing work looks like over time.\rNote: We understand that'
+      ' this timeline is based only on what you know r\'now and that'
+      ' circumstances change.\r\n'),
+    'expected_count': 105,
+    'simple': True
+  },
+  {
+    'text': (u'This gránt will provide funding for two years. While we know it'
+      u' can be difficult to predict your work beyond a year, please give us an'
+      u' idea of what the second year might look like.\t - What overall goals and'
+      u' strategies do you ø∆ forecast in the second year?\t* How will the second year'
+      u' of this grant build on your work in the first year?\r'),
+    'expected_count': 64,
+    'simple': False
+  }
+]
+
 
 class WordLimit(TestCase):
 
@@ -12,17 +49,24 @@ class WordLimit(TestCase):
     self.assertTrue(validator.compare(9, 3))
     self.assertFalse(validator.compare(0, 0))
 
-  def test_clean_simple(self):
-    """ Should return word count """
+  def test_clean(self):
     validator = WordLimitValidator(limit_value=5)
-    self.assertEqual(validator.clean('a b c def'), 4)
-    self.assertEqual(validator.clean('a ba-lw c\ndef a\r  "e, r" u badsads askdk j j '), 12)
+    for example in EXAMPLES:
+      word_count = validator.clean(example['text'])
+      prior_word_count = simple_clean(example['text'])
 
-  def test_call(self):
+      self.assertEqual(word_count, example['expected_count'])
+
+      if example['simple']:
+        self.assertEqual(word_count, prior_word_count)
+      else:
+        self.assertLess(word_count, prior_word_count)
+
+  def test_call_exception(self):
     """ Should raise exception when word count is over limit """
     validator = WordLimitValidator(limit_value=5)
 
-    validator('a b c')
+    validator('* a - b - c,') # should not raise exception
 
     self.assertRaisesRegexp(
         ValidationError,
